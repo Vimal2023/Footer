7cf0303d790613f7ec8737d0687dca1327cb005d
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-04-18 17:24:02 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter} from 'react-router-dom';
+
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        
+        <Footer />
+      </BrowserRouter>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <div className="Footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-lg-5 col-12 ft-1">
+                            <h3><span>Empower</span>U</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum ea quo ex ullam laboriosam magni totam, facere eos iure voluptate.</p>
+                            <div className="footer-icons">
+                                <i class="fa-brands fa-facebook"></i>
+                                <i class="fa-brands fa-twitter"></i>
+                                <i class="fa-brands fa-instagram"></i>
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-lg-3 col-12 ft-2">
+                            <h5>Quick Links</h5>
+                            <ul>
+                                <li className="nav-item">
+                                    <a className="" href="/">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="" href="/">Create a project</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="" href="/">All Projects</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="" href="/">Services</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="" href="/">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-md-6 col-lg-4 col-12 ft-3">
+                            <h5>Quick Links</h5>
+                            <p><i class="fa-solid fa-phone-volume"></i> +91 173314</p>
+                            <p><i class="fa-solid fa-envelope"></i> empoweru23@gmail.com</p>
+                            <p><i class="fa-solid fa-paper-plane"></i> Kalyani, Nadia</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
